b97c9ca0b98142d48faa814b63de2a92d72ad4ce
maoya12345 <jannatulmaoya456@gmail.com>
2026-07-12 10:49:58 +0600

    project start

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+//import connectDB from './config/db';
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//connectDB();
+
+app.get('/', (req, res) => {
+    res.send("API is running...");
+});
+
+app.listen(5000, () => 
+    console.log("Server running on port 5000"));
