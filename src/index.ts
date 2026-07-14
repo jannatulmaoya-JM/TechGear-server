d8bd46dba20d2c29d3cbb55f1ff12979fa0766e4
maoya12345 <jannatulmaoya456@gmail.com>
2026-07-15 02:43:52 +0600

    get pai product id

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,88 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-//import connectDB from './config/db';
+import { MongoClient, ServerApiVersion,  } from 'mongodb';
+import jwt from 'jsonwebtoken';
 
 dotenv.config();
-const app = express();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
+const { ObjectId } = require('mongodb');
+const uri = process.env.MONGODB_URI as string;
+const JWT_SECRET = process.env.JWT_SECRET || "JWT_SECRET=super_secret_key_88f92b7c1a4e5d6f3g9h2j5k8l0n1m4q";
 
-//connectDB();
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-app.get('/', (req, res) => {
-    res.send("API is running...");
+async function run() {
+  try {
+    await client.connect();
+    console.log("Successfully connected to MongoDB!");
+
+   const db = client.db("Tech-Gear");
+   const productsCollection = db.collection("Products");
+
+    app.get('/api/products', async (req: Request, res: Response) => {
+      try {
+        const products = await productsCollection.find({}).toArray();
+        // console.log("Found products:", products);
+        res.send(products);
+      } catch (err) {
+        res.status(500).send({ message: "Error fetching products" });
+      }
+    });
+  
+    app.get('/api/products/:id', async (req: Request, res: Response) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const product = await productsCollection.findOne(query);
+        
+        if (!product) {
+          return res.status(404).send({ message: "Product not found" });
+        }
+        
+        res.send(product);
+      } catch (err) {
+        res.status(500).send({ message: "Error fetching product details" });
+      }
+    });
+    
+    app.post('/api/products', async (req: Request, res: Response) => {
+      try {
+        const newProduct = req.body;
+        const result = await productsCollection.insertOne(newProduct);
+        res.send(result);
+      } catch (err) {
+        res.status(500).send({ message: "Error adding product" });
+      }
+    });
+
+    // 3. JWT Authentication API (Login)
+    app.post('/api/auth/login', (req: Request, res: Response) => {
+      const { email } = req.body;
+      
+      const token = jwt.sign({ email }, JWT_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    });
+
+  } catch (err) {
+    console.error("MongoDB Connection Error:", err);
+  }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send("API is running...");
 });
 
 app.listen(5000, () => 
-    console.log("Server running on port 5000"));
+  console.log("Server running on port 5000"));
